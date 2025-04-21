bc881baee525b6e73c45e1c1531fd09d203878f7
Polygzz <139706982+Polygzz@users.noreply.github.com>
2025-04-21 11:20:55 -0700

    Initial commit from your app

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  buildOptions: {
+    site: 'https://soccerpro.netlify.app/',  // Set to your site's URL or production URL
+  },
 });
